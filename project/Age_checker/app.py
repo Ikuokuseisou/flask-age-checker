ada8302c10c38cdf10bf0499e0ae3d58af5f7bf3
Ikuokuseisou <trip_3320161315@yahoo.co.jp>
2026-03-14 01:46:12 +0900

    update age checker:validation, UI improvement and screenshots

--- project/Age_checker/app.py
+++ project/Age_checker/app.py
@@ -2,6 +2,19 @@ from flask import Flask,request,render_template,redirect,url_for
 
 app = Flask(__name__)
 
+def classify_age(age):
+    if age <= 14 :
+        return "子供"
+    elif age <= 18 :
+        return  "少年"
+    elif age <= 30 :
+
+        return "青年"
+    elif age  <= 60:
+        return "中年"
+    else :
+        return "高齢者"
+
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -12,10 +25,10 @@ def submit():
     try:
         age = int(age)
     except ValueError :
-       return "数字を入力してください"
+       return render_template("index.html", error = "数字を入力してください" )
     
     if age < 0 or age > 120 :
-        return "実年齢を入力してください"
+        return render_template("index.html",error = "実年齢を入力してください")
 
     return redirect(url_for("result",age = age))
 
@@ -24,7 +37,9 @@ def result():
     age = request.args.get("age")
     age = int(age)
 
-    return render_template("result.html",age = age)
+    category = classify_age(age)
+
+    return render_template("result.html",age = age,category = category)
  
 if __name__ == "__main__":
     app.run(debug=True)
